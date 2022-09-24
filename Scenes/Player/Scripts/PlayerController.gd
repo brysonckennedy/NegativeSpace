fe507c4455feb7303967a1dc4be8fe31b67290ce
brysonckennedy <brysonckennedy@gmail.com>
2022-09-24 17:30:52 -0500

    New Branch + Player Controller Submit
    I added the player controller and that's it

--- Scenes/Player/Scripts/PlayerController.gd
+++ Scenes/Player/Scripts/PlayerController.gd
@@ -1,15 +1,57 @@
 extends KinematicBody2D
 
-var ShipVelocity : Vector2 = Vector2.ZERO
-var Gravity = 0
-
-
-
-
-func _ready():
-	pass # Replace with function body.
-
-
+var velocityX = 0
+var velocityY = 0
+var terminalVelocity = 500
+var accerleration = 20
+var decerleration = 20
 
 func _physics_process(delta):
+	print(getDirection().x)
 	
+	#X Direction Movement
+	if getDirection().x > 0:
+		if velocityX < terminalVelocity:
+			velocityX =  velocityX + (delta * 60 * accerleration) #multiply by action strength to have increase in acceleration with stick
+	if getDirection().x < 0:
+		if velocityX > -terminalVelocity:
+			velocityX =  velocityX + (delta * 60 * -accerleration)
+	#Deceleration X
+	if getDirection().x == 0:
+		if velocityX > 0:
+			velocityX = velocityX + (delta * 60 * -decerleration)
+		if velocityX < 0:
+			velocityX = velocityX + (delta * 60 * decerleration)
+
+	#Y Direction Movement
+	if getDirection().y > 0:
+		if velocityY < terminalVelocity:
+			velocityY =  velocityY + (delta * 60 * accerleration)
+	if getDirection().y < 0:
+		if velocityY > -terminalVelocity:
+			velocityY =  velocityY + (delta * 60 * -accerleration)
+	#Deceleration Y
+	if getDirection().y == 0:
+		if velocityY > 0:
+			velocityY = velocityY + (delta * 60 * -decerleration)
+		if velocityY < 0:
+			velocityY = velocityY + (delta * 60 * decerleration)
+	
+	
+	
+	if _magnitude(Vector2(velocityX, velocityY)) < terminalVelocity:
+		move_and_slide(Vector2(velocityX, velocityY), Vector2.UP)
+	else:
+		move_and_slide(Vector2(velocityX, velocityY).normalized() * terminalVelocity, Vector2.UP)
+
+func getDirection():
+	var RightDirection = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
+	var UpDirection = Input.get_action_strength("MoveDown") - Input.get_action_strength("MoveUp") 
+	var DirectionVector : Vector2 = Vector2(RightDirection, UpDirection)
+	return DirectionVector
+
+func _magnitude(v):
+	return sqrt(pow(v.x, 2) + pow(v.y, 2))
+
+func _normalize(v):
+	return v / _magnitude(v)
